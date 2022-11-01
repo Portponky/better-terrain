eb84937247824d3ff36c95de4d8f487aa0648975
Portponky <portponky@gmail.com>
2022-11-01 15:00:34 +0000

    Fix peering types of hexagonal grids

--- addons/better-terrain/TerrainData.gd
+++ addons/better-terrain/TerrainData.gd
@@ -6,9 +6,9 @@ const terrain_peering_square_tiles := [0, 3, 4, 7, 8, 11, 12, 15]
 const terrain_peering_square_vertices := [3, 7, 11, 15]
 const terrain_peering_isometric_tiles := [1, 2, 5, 6, 9, 10, 13, 14]
 const terrain_peering_isometric_vertices := [1, 5, 9, 13]
-const terrain_peering_horiztonal_tiles := [0, 2, 3, 5, 6, 7, 8, 10, 11, 13, 14, 15]
+const terrain_peering_horiztonal_tiles := [0, 2, 6, 8, 10, 14]
 const terrain_peering_horiztonal_vertices := [3, 5, 7, 11, 13, 15]
-const terrain_peering_vertical_tiles := [1, 2, 3, 4, 6, 7, 9, 10, 11, 12, 14, 15]
+const terrain_peering_vertical_tiles := [2, 4, 6, 10, 12, 14]
 const terrain_peering_vertical_vertices := [1, 3, 7, 9, 11, 15]
 
 
