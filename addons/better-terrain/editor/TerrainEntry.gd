63f57817d51454543865ba304fe5d477f161efd4
torcado <torcado@yahoo.com>
2023-07-16 01:53:36 -0400

    terrain icons

---
+++ addons/better-terrain/editor/TerrainEntry.gd
@@ -0,0 +1,98 @@
+@tool
+extends PanelContainer
+
+signal select(index)
+
+@onready var color_panel := %Color
+@onready var terrain_icon_slot := %TerrainIcon
+@onready var type_icon_slot := %TypeIcon
+@onready var name_label := %Name
+
+var selected := false
+
+var tileset:TileSet
+var terrain:Dictionary
+
+var _terrain_texture:Texture2D
+var _terrain_texture_rect:Rect2i
+var _icon_draw_connected := false
+
+@onready var terrain_icons := [
+	load("res://addons/better-terrain/icons/MatchTiles.svg"),
+	load("res://addons/better-terrain/icons/MatchVertices.svg"),
+	load("res://addons/better-terrain/icons/NonModifying.svg"),
+]
+
+func _ready():
+	update()
+
+func update():
+	if !terrain:
+		return
+	if !tileset:
+		return
+	
+	name_label.text = terrain.name
+	
+	var style:StyleBoxFlat = color_panel.get_theme_stylebox("panel").duplicate()
+	style.bg_color = terrain.color
+	color_panel.add_theme_stylebox_override("panel", style)
+	
+	type_icon_slot.texture = terrain_icons[terrain.type]
+	
+	var has_icon = false
+	if terrain.has("icon"):
+		if terrain.icon.has("path") and not terrain.icon.path.is_empty():
+			terrain_icon_slot.texture = load(terrain.icon.path)
+			_terrain_texture = null
+			terrain_icon_slot.queue_redraw()
+			has_icon = true
+		elif terrain.icon.has("source_id"):
+			var source := tileset.get_source(terrain.icon.source_id) as TileSetAtlasSource
+			var coord := terrain.icon.coord as Vector2i
+			var rect := source.get_tile_texture_region(coord, 0)
+			_terrain_texture = source.texture
+			_terrain_texture_rect = rect
+			terrain_icon_slot.queue_redraw()
+			has_icon = true
+	
+	if not has_icon:
+		var tiles = BetterTerrain.get_tile_sources_in_terrain(tileset, get_index())
+		if tiles.size() > 0:
+			var source := tiles[0].source as TileSetAtlasSource
+			var coord := tiles[0].coord as Vector2i
+			var rect := source.get_tile_texture_region(coord, 0)
+			_terrain_texture = source.texture
+			_terrain_texture_rect = rect
+			terrain_icon_slot.queue_redraw()
+	
+	if _terrain_texture:
+		terrain_icon_slot.texture = null
+	
+	if not _icon_draw_connected:
+		terrain_icon_slot.connect("draw", func():
+			if _terrain_texture:
+				terrain_icon_slot.draw_texture_rect_region(_terrain_texture, Rect2i(0,0, 44, 44), _terrain_texture_rect)
+		)
+		_icon_draw_connected = true
+
+
+func set_selected(value:bool = true):
+	selected = value
+	if value:
+		select.emit(get_index())
+	queue_redraw()
+
+func _draw():
+	if selected:
+		draw_rect(Rect2(Vector2.ZERO, get_rect().size), Color(0.15, 0.70, 1, 0.3))
+
+
+func _on_focus_entered():
+	queue_redraw()
+	selected = true
+	select.emit(get_index())
+
+
+func _on_focus_exited():
+	queue_redraw()
