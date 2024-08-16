1c66c9ce43d8e5150f28e532e0f6b8e983e4734f
Portponky <portponky@gmail.com>
2024-08-16 23:01:28 +0100

    Fix categories not showing in terrain properties

--- addons/better-terrain/editor/TerrainProperties.gd
+++ addons/better-terrain/editor/TerrainProperties.gd
@@ -1,15 +1,7 @@
 @tool
 extends ConfirmationDialog
 
-@onready var name_edit: LineEdit = %NameEdit
-@onready var color_picker: ColorPickerButton = %ColorPicker
-@onready var icon_edit: LineEdit = %IconEdit
-@onready var type_option: OptionButton = %TypeOption
-@onready var category_label: Label = $GridContainer/CategoryLabel
-@onready var category_container: ScrollContainer = $GridContainer/CategoryContainer
-@onready var category_layout: VBoxContainer = %CategoryLayout
-
-@onready var category_icon := load("res://addons/better-terrain/icons/NonModifying.svg")
+var category_icon := load("res://addons/better-terrain/icons/NonModifying.svg")
 
 const CATEGORY_CHECK_ID = &"category_check_id"
 
@@ -39,8 +31,8 @@ var terrain_categories : Array: set = set_categories, get = get_categories
 # category is name, color, id
 func set_category_data(options: Array) -> void:
 	if !options.is_empty():
-		category_label.show()
-		category_container.show()
+		%CategoryLabel.show()
+		%CategoryContainer.show()
 	
 	for o in options:
 		var c = CheckBox.new()
@@ -55,7 +47,7 @@ func set_category_data(options: Array) -> void:
 		c.add_theme_color_override(&"icon_pressed_color", o.color)
 		
 		c.set_meta(CATEGORY_CHECK_ID, o.id)
-		category_layout.add_child(c)
+		%CategoryLayout.add_child(c)
 
 
 func set_categories(ids : Array):
@@ -67,7 +59,7 @@ func get_categories() -> Array:
 	var result := []
 	if terrain_type == BetterTerrain.TerrainType.CATEGORY:
 		return result
-	for c in category_layout.get_children():
+	for c in %CategoryLayout.get_children():
 		if c.button_pressed:
 			result.push_back(c.get_meta(CATEGORY_CHECK_ID))
 	return result
